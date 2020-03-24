0afb15df3d64227c885a2609e00bbf6b218e70ab
yawarakakuma's copy <43271603+escapisms@users.noreply.github.com>
2020-03-25 00:39:53 +0900

    create auth

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,26 @@
 <template>
-  <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6>
-      <v-card>
-        <v-card-title class="headline">
-          Wake Wars
-        </v-card-title>
-        <v-card-text>
-          <p>朝7時に起きてね</p>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn @click="signIn">signIn</v-btn>
-+          <p>isAuthenticated: {{ isAuthenticated }}</p>
-          <v-btn color="primary" nuxt to="/wakeup">
-            起きたよ
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
+  <v-app>
+    <v-layout column justify-center align-center>
+      <v-flex xs12 sm8 md6>
+        <v-card>
+          <v-card-title class="headline">
+            Wake Wars
+          </v-card-title>
+          <v-card-text>
+            <p>朝7時に起きてね</p>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer />
+            <v-btn nuxt to="/login">signIn</v-btn>
+            <v-btn color="primary" nuxt to="/wakeup">
+              起きたよ
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-app>
 </template>
 
 <script>
-import { mapGetters, mapActions } from 'vuex'
-export default {
-  computed: {
-    ...mapGetters('user', ['isAuthenticated'])
-  },
-  methods: {
-    ...mapActions('user', ['signIn'])
-  }
-}
 </script>
---
+++ pages/login.vue
@@ -0,0 +1,41 @@
+/* eslint-disable */
+<template>
+  <v-app>
+    <v-layout column justify-center align-center>
+      <v-flex xs12 sm8 md6>
+        <v-container>
+          <div id="firebaseui-auth-container" />
+        </v-container>
+      </v-flex>
+    </v-layout>
+  </v-app>
+</template>
+
+<script>
+export default {
+  mounted () {
+    const firebase = require('firebase')
+    const firebaseui = require('firebaseui')
+    require('firebaseui/dist/firebaseui.css')
+
+    const uiConfig = {
+      signInSuccessUrl: '/',
+      signInOptions: [
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID
+      ]
+    }
+    const firebaseConfig = {
+      apiKey: 'AIzaSyDs5OmX7cGrkGDp4d59bceaDgBtmEzVn0A',
+      authDomain: 'wakewars-22423.firebaseapp.com',
+      databaseURL: 'https://wakewars-22423.firebaseio.com',
+      projectId: 'wakewars-22423',
+      storageBucket: 'wakewars-22423.appspot.com',
+      messagingSenderId: '298606668680'
+    }
+    firebase.initializeApp(firebaseConfig)
+
+    const ui = new firebaseui.auth.AuthUI(firebase.auth())
+    ui.start('#firebaseui-auth-container', uiConfig)
+  }
+}
+</Script>
